5c1a5b176fe037c1d732629206dad8f9fb756b78
Kim Young Jik <capbang93@gmail.com>
2023-03-26 10:53:33 +0900

    14 프론트+백엔드통합

--- src/App_back.js
+++ src/App_back.js
@@ -3,7 +3,6 @@ import Todo from './Todo';
 import AddTodo from './AddTodo';
 import {Paper, List, Container} from "@material-ui/core";
 import './App.css';
-import { call } from './service/ApiService';
 
 class App extends React.Component {
   constructor(props) {  //매개변수 props 생성자
@@ -14,26 +13,42 @@ class App extends React.Component {
   }
   // add 함수 추가
   add = (item) => {
-    call("/todo","POST",item).then((response) =>
-      this.setState({items:response.data})
-    );
-  }
-  
-  delete = (item) => {
-    call("/todo","DELETE",item).then((response) =>
-      this.setState({items:response.data})
-    );    
-  }
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; //key를 위한 id 추가
+    item.done = false;
+    thisItems.push(item);
+    this.setState({items:thisItems}); //update state
+    console.log("items:",this.state.items);
 
-  update = (item) => {
-    call("/todo","PUT",item).then((response) =>
-      this.setState({items:response.data})
-    );    
+  }
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items:newItems}, () => {
+      //디버깅 콜백
+      console.log("Update Items : ", this.state.items)
+    });
   }
 
   componentDidMount() {
-    call("/todo", "GET", null).then((response) =>
-      this.setState( {items:response.data })
+    const requestOptions = {
+      method: "GET",
+      headers: {"Content-Type:": "application/json"},
+    };
+    
+    fetch("http://localhost:8080/todo", requestOptions)
+    .then((response) => response.json())
+    .then(
+      (response) => {
+        this.setState({
+          items: response.data,
+        });
+      },
+      (error) => {
+        this.setState({
+          error,
+        });
+      }
     );
   }
 
@@ -44,7 +59,7 @@ class App extends React.Component {
       <Paper style={{margin:16}}>
         <List>
         { this.state.items.map((item,idx) => (
-        <Todo item={item} key={item.id} delete={this.delete} update={this.update} />
+        <Todo item={item} key={item.id} delete={this.delete} />
         ))}
         </List>
       </Paper>
