6d542e33a6da6c7ea5faf6da98fe8e7639f81893
Kim Young Jik <capbang93@gmail.com>
2023-03-26 12:26:11 +0900

    프론트+백엔드 통합 로그인 페이지 뜨는거 확인

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,22 +1,30 @@
-import { API_BASE_URL} from "../app-config";
+import { API_BASE_URL } from "../app-config";
 
-export function call(api,method, request) {
+export function call(api, method, request) {
     let options = {
         headers: new Headers({
             "Content-Type": "application/json",
         }),
-        url:API_BASE_URL +  api,
+        url: API_BASE_URL + api,
         method: method,
     };
-    if(request) {
+    if (request) {
         options.body = JSON.stringify(request);
     }
-    return fetch(options.url, options).then((response) =>
-    response.json().then((json) => {
-        if(!response.ok) {
-            return Promise.reject(json);
-        }
-        return json;
-    })
-    );
+    return fetch(options.url, options)
+        .then((response) =>
+            response.json().then((json) => {
+                if (!response.ok) {
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        )
+        .catch((error) => {
+            console.log(error.status);
+            if(error.status === 403) {
+               window.location.href = "/login";
+            }
+            return Promise.reject(error);
+        });
 }
